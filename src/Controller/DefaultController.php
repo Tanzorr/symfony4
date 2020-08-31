24bdaa274e4052783f5c1cec876982edd02a4a33
alex84 <alexx1984@ukr.net>
2020-08-31 17:28:13 +0300

    addet first services

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Services\GiftService;
+use Couchbase\GeoBoundingBoxSearchQuery;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,12 +13,17 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="default")
      */
-    public function index()
+    public function index(GiftService $gifts)
     {
        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+       if(!isset($users)) {
+           $users = [];
+       }
+
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
-            'users' => $users
+            'users' => $users,
+            'random_gift'=>$gifts->gifts,
         ]);
     }
 }
